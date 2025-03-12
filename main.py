9f40d8aab43acd978bf4a0928d852e42520cae0a
Neizan Roggie <neizanroggie7@gmail.com>
2025-03-12 09:22:01 +0100

    lab2

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ def crear_laberinto_resoluble(filas=11, columnas=15):
     if columnas % 2 == 0:
         columnas += 1
 
-
     laberinto = [['#' for _ in range(columnas)] for _ in range(filas)]
-
 
     laberinto[1][0] = '#E'
     laberinto[filas - 2][columnas - 2] = 'S'
-
 
     def generar_camino(x, y):
         direcciones = [(0, 2), (2, 0), (0, -2), (-2, 0)]
@@ -24,7 +21,6 @@ def crear_laberinto_resoluble(filas=11, columnas=15):
                 laberinto[x + dx // 2][y + dy // 2] = ' '
                 laberinto[nx][ny] = ' '
                 generar_camino(nx, ny)
-
 
     laberinto[1][1] = ' '
     generar_camino(1, 1)
@@ -41,8 +37,55 @@ def crear_laberinto_resoluble(filas=11, columnas=15):
 def imprimir_laberinto(laberinto):
     for fila in laberinto:
         print("".join(fila))
+    print()
+
+
+def agente_reactivo(laberinto):
+
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto[i])):
+            if laberinto[i][j] == '#E':
+                agente_fila, agente_columna = i, j
+                break
+
+
+    direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+    while laberinto[agente_fila][agente_columna] != 'S':
+        # Marcar la casilla actual como visitada
+        if laberinto[agente_fila][agente_columna] != '#E':
+            laberinto[agente_fila][agente_columna] = '·'
+
+
+        movido = False
+        for direccion in direcciones:
+            nueva_fila = agente_fila + direccion[0]
+            nueva_columna = agente_columna + direccion[1]
+
+
+            if 0 <= nueva_fila < len(laberinto) and 0 <= nueva_columna < len(laberinto[0]):
+                if laberinto[nueva_fila][nueva_columna] == ' ' or laberinto[nueva_fila][nueva_columna] == 'S':
+                    agente_fila, agente_columna = nueva_fila, nueva_columna
+                    movido = True
+                    break
+
+
+        if not movido:
+            print("El agente no puede encontrar la salida.")
+            break
+
+
+        imprimir_laberinto(laberinto)
+
+    if laberinto[agente_fila][agente_columna] == 'S':
+        print("¡El agente ha encontrado la salida!")
 
 
 if __name__ == "__main__":
     lab = crear_laberinto_resoluble()
+    print("Laberinto inicial:")
     imprimir_laberinto(lab)
+
+    print("Recorrido del agente:")
+    agente_reactivo(lab)
